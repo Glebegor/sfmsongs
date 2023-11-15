17a70d3fd62d0172c28e5e32ed2b09683e0c2538
Hlib Arseniuk <arseniukh@spseplzen.cz>
2023-11-15 17:16:45 +0100

    Need to create over scroll list of music

--- package/layouts/songsList.go
+++ package/layouts/songsList.go
@@ -30,6 +30,7 @@ func (o *SongListLayout) Init(gtx layout.Context, th *material.Theme, songsArray
 	}
 
 	var list widget.List
+	list.ScrollToEnd = true
 	list.Axis = layout.Vertical
 
 	return layout.Flex{
@@ -42,11 +43,20 @@ func (o *SongListLayout) Init(gtx layout.Context, th *material.Theme, songsArray
 					Axis:    layout.Vertical,
 					Spacing: layout.SpaceAround,
 				}.Layout(gtx,
-					layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
-						return list.Layout(gtx, len(songs), func(gtx layout.Context, index int) layout.Dimensions {
-							return songs[index].Layout(th, gtx) // Pass the gtx to the Layout function
-						})
-					}),
+					layout.Flexed(1,
+						func(gtx layout.Context) layout.Dimensions {
+							return layout.UniformInset(unit.Dp(16)).Layout(gtx,
+								func(gtx layout.Context) layout.Dimensions {
+									return list.List.Layout(gtx, len(songsArray)-1,
+										func(gtx layout.Context, index int) layout.Dimensions {
+											return songs[index].Layout(th, gtx)
+										},
+									)
+								},
+							)
+
+						},
+					),
 				)
 			},
 		),
@@ -67,18 +77,33 @@ func (item SongsListItem) Layout(th *material.Theme, gtx layout.Context) layout.
 	}.Layout(gtx,
 		layout.Rigid(
 			func(gtx layout.Context) layout.Dimensions {
-				return material.H6(th, "Song name").Layout(gtx)
-			},
-		),
-		layout.Rigid(
-			func(gtx layout.Context) layout.Dimensions {
-				return material.H6(th, "02:20").Layout(gtx)
-			},
-		),
-		layout.Rigid(
-			func(gtx layout.Context) layout.Dimensions {
-				var btn widget.Clickable
-				return material.Button(th, &btn, "Play").Layout(gtx)
+				return layout.Inset{
+					Top:    unit.Dp(8),
+					Right:  unit.Dp(8),
+					Bottom: unit.Dp(8),
+					Left:   unit.Dp(8),
+				}.Layout(gtx,
+					func(gtx layout.Context) layout.Dimensions {
+						return layout.Flex{}.Layout(gtx,
+							layout.Rigid(
+								func(gtx layout.Context) layout.Dimensions {
+									return material.Label(th, unit.Sp(10), item.MusicPath).Layout(gtx)
+								},
+							),
+							layout.Rigid(
+								func(gtx layout.Context) layout.Dimensions {
+									return material.Label(th, unit.Sp(10), "02:33").Layout(gtx)
+								},
+							),
+							layout.Rigid(
+								func(gtx layout.Context) layout.Dimensions {
+									var btn widget.Clickable
+									return material.Button(th, &btn, "Play").Layout(gtx)
+								},
+							),
+						)
+					},
+				)
 			},
 		),
 	)
